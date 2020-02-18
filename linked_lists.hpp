57289a85aacac9147e4e3bedac7d4abc39cf2849
Paulina Khew <paulinakhew12345@hotmail.com>
2020-02-18 13:47:22 -0500

    :art: Move function declarations into header file
     #1

---
+++ linked_lists.hpp
@@ -0,0 +1,28 @@
+//
+// Created by Paulina Khew on 2020-02-18.
+//
+
+#ifndef CPP_LINKED_LISTS_LINKED_LISTS_HPP
+#define CPP_LINKED_LISTS_LINKED_LISTS_HPP
+
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+struct LinkedList {
+    Node* head;
+
+public:
+    void InsertNode(LinkedList* list, struct Node* newNode);
+    void PrintList(LinkedList* list);
+    bool FindValue (LinkedList* list, int value);
+    void DeleteLast(LinkedList* list);
+    };
+
+
+#endif //CPP_LINKED_LISTS_LINKED_LISTS_HPP
--- main.cpp
+++ main.cpp
@@ -1,86 +1,76 @@
-#include <iostream>
-using namespace std;
+#include "linked_lists.hpp"
 
-struct Node {
-    int data;
-    Node* next;
-};
-
-struct LinkedList {
-    Node* head;
-public:
-    void InsertNode(LinkedList* list, struct Node* newNode) {
-        // If the list is empty, create a new head node
-        if (list->head == NULL){
-            list -> head = newNode;
-            newNode -> next = NULL;
-        }
-        // If the list is not empty, start at the head
-        // and follow the links until you find the end of the list
-        else {
-            Node * end = list -> head;
-            while (end -> next != NULL) {
-                end = end -> next;
-            }
-
-            // Then, set the end of the list to point to the new node
-            end -> next = newNode;
-            newNode -> next = NULL;
-        }
+void LinkedList::InsertNode(LinkedList* list, struct Node* newNode) {
+    // If the list is empty, create a new head node
+    if (list->head == NULL){
+        list -> head = newNode;
+        newNode -> next = NULL;
     }
-
-    void PrintList(LinkedList* list) {
-        if (list == NULL)
-            return;
-        cout << "(";
-        Node* node = list -> head;
-        while (node != NULL) {
-            cout << "[" << node -> data << "]";
-            node = node -> next;
-            if (node != NULL)
-                cout << "->";
+    // If the list is not empty, start at the head
+    // and follow the links until you find the end of the list
+    else {
+        Node * end = list -> head;
+        while (end -> next != NULL) {
+            end = end -> next;
         }
-        cout << ")\n";
+
+        // Then, set the end of the list to point to the new node
+        end -> next = newNode;
+        newNode -> next = NULL;
     }
+}
 
-    bool FindValue (LinkedList* list, int value) {
-        if (list == NULL)
-            return false;
-        Node* node = list -> head;
-        while (node != NULL) {
-            if (node -> data == value)
-                return true;
-            node = node -> next;
-        }
+void LinkedList::PrintList(LinkedList* list) {
+    if (list == NULL)
+        return;
+    cout << "(";
+    Node* node = list -> head;
+    while (node != NULL) {
+        cout << "[" << node -> data << "]";
+        node = node -> next;
+        if (node != NULL)
+            cout << "->";
+    }
+    cout << ")\n";
+}
+
+bool LinkedList::FindValue (LinkedList* list, int value) {
+    if (list == NULL)
         return false;
+    Node* node = list -> head;
+    while (node != NULL) {
+        if (node -> data == value)
+            return true;
+        node = node -> next;
     }
+    return false;
+}
 
-    void DeleteLast(LinkedList* list) {
-        if (list == NULL)
-            return;
+void LinkedList::DeleteLast(LinkedList* list) {
+    if (list == NULL)
+        return;
 
-        // Check to make sure that there are at least two elements in the list
-        if (list -> head != NULL && list -> head -> next != NULL) {
-            // When this loop finishes, newLast will point to the node
-            // that will be second-last in the list.
-            Node* newLast = list -> head;
-            while (newLast -> next -> next != NULL) {
-                newLast = newLast -> next;
-            }
-            delete newLast -> next;
-            newLast -> next = NULL;
-
-        // If there is only 1 Node in the list, we just have to delete that node,
-        // and set head to NULL
-        } else if (list -> head != NULL) {
-            delete list -> head;
-            list -> head = NULL;
+    // Check to make sure that there are at least two elements in the list
+    if (list -> head != NULL && list -> head -> next != NULL) {
+        // When this loop finishes, newLast will point to the node
+        // that will be second-last in the list.
+        Node* newLast = list -> head;
+        while (newLast -> next -> next != NULL) {
+            newLast = newLast -> next;
         }
+        delete newLast -> next;
+        newLast -> next = NULL;
 
-        // The only remaining case is that the list was empty
-        // to begin with, in which case we should do nothing
+    // If there is only 1 Node in the list, we just have to delete that node,
+    // and set head to NULL
+    } else if (list -> head != NULL) {
+        delete list -> head;
+        list -> head = NULL;
     }
-};
+
+    // The only remaining case is that the list was empty
+    // to begin with, in which case we should do nothing
+}
 
 int main() {
     LinkedList* list;
