e7604061bb26cc7220eddbe1219675a716e45e3e
Paulina Khew <paulinakhew12345@hotmail.com>
2020-02-23 21:38:48 -0500

    :bug: Fix binary search function
     #5

--- binary_search.cpp
+++ binary_search.cpp
@@ -2,19 +2,27 @@
 // Created by Paulina Khew on 2020-02-20.
 //
 
-int get_index_binary_search(int arr[], int left, int right, int num) {
-    if (left <= right) {
-        int mid = (left + right)/2;
-        if (arr[mid] == num)
-            return mid ;
-        else if (arr[left] == num)
-            return left;
-        else if (arr[right] == num)
-            return right;
-        else if (arr[mid] > num)
-            return get_index_binary_search(arr, left, mid-1, num);
-        else if (arr[mid] < num)
-            return get_index_binary_search(arr, mid+1, right, num);
+int get_index_binary_search(int arr[], int first, int last, int num) {
+    int middle = (first+last)/2;
+    while (first <= last)
+    {
+        if(arr[middle] == num)
+        {
+            return middle;
+        }
+        else if(arr[middle] < num)
+        {
+            first = middle + 1;
+
+        }
+        else {
+            last = middle - 1;
+        }
+        middle = (first + last)/2;
     }
-    return -1;
+    if(first > last)
+    {
+        return -1;
+    }
+    return 0;
 }
